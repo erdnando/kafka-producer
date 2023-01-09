7842cd079f33c7e14e11a5759e976dfce6d8f671
erdnando <erdnando@gmail.com>
2023-01-09 11:01:12 -0600

    localhost 9093

--- src/main/java/com/asb/example/KafkaConfig.java
+++ src/main/java/com/asb/example/KafkaConfig.java
@@ -17,7 +17,7 @@ public class KafkaConfig {
 	@Bean
 	ProducerFactory<String, String> producerFactory() {
 		Map<String, Object> config = new HashMap<>();
-		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "my-cluster-kafka-listener1-bootstrap-amq-streams.apps-crc.testing:9094");
+		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "my-cluster-kafka-bootstrap.amq-streams.svc:9092");
 		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		return new DefaultKafkaProducerFactory<String, String>(config);
