8b3629ba036db4f5349cbe2dd773c38a9fb941e6
erdnando <erdnando@gmail.com>
2023-02-24 09:01:11 -0600

    apuntando claro externo puerto 9092

--- src/main/java/com/asb/example/KafkaConfig.java
+++ src/main/java/com/asb/example/KafkaConfig.java
@@ -18,21 +18,12 @@ public class KafkaConfig {
 	ProducerFactory<String, String> producerFactory() {
 		Map<String, Object> config = new HashMap<>();
 		  
-		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "my-cluster-kafka-bootstrap-amq-streams.apps.claro.co:443");
-		//config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "my-cluster-kafka-bootstrap.amq-streams.svc:9091");
+		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "my-cluster-kafka-bootstrap.amq-streams.svc:9092");//ok
+		//config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "my-cluster-kafka-external-bootstrap.amq-streams.svc:9094");
+		
 		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		config.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, 70000);
-		/*config.put("enable.auto.commit", "false");
-		config.put("auto.offset.reset", "earliest");
-		config.put("security.protocol", "SASL_PLAINTEXT");
-		config.put("basic.auth.credentials.source", "USER_INFO");
-		config.put("basic.auth.user.info", "${user}:${pass}");
-		config.put("sasl.kerberos.service.name", "kafka");
-		config.put("auto.register.schemas", "false");
-		config.put("schema.registry.url", "${https://your_url}");
-		config.put("schema.registry.ssl.truststore.location", "client_truststore.jks");
-		config.put("schema.registry.ssl.truststore.password", "${password}");*/
+		
 		
 		return new DefaultKafkaProducerFactory<String, String>(config);
 		
